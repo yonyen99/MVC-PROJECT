9ae993d74215406ee775918e769e9ac8272f459f
yon yen <yon.yen@pnc.lan>
2019-12-06 08:44:09 +0700

    viewsdetail

--- views/dashboard/homedashboard.php
+++ views/dashboard/homedashboard.php
@@ -2,3 +2,4 @@
 <a href="views/class/wep_a.php">Web A</a>
 <a href="views/class/wep_b.php">Web b</a>
 <a href="views/class/sna.php">sna</a>
+<a href="views/viewdetail.php">viewdetail</a>
---
+++ views/viewdetail.php
@@ -0,0 +1,2 @@
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css">
+<h1  class ="text-center">Welcome to Viewdetail page</h1>
